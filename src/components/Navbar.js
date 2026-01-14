5ed4a8615cedb2f7d88316758b322d623d998e49
kgt-it <khushisaraswat69@gmail.com>
2026-01-13 18:52:04 -0800

    Initial commit - TextMaster React App

---
+++ src/components/Navbar.js
@@ -0,0 +1,78 @@
+//import styled from 'styled-components';
+
+
+
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      >
+        <a className="navbar-brand pl-3" href="/">
+          <img src = "/TextMaster.png" alt="Text Master"  width="30"  height="30" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <div className={`custom-control custom-switch text-${props.mode==='light'?'dark':'light'}`}>
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customSwitch1"
+                onClick={props.toggleMode}
+              />
+              <label className="custom-control-label" htmlFor="customSwitch1">
+                Enable Dark Mode
+              </label>
+            </div>
+            {/* <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button> */}
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+//Navbar.propTypes = {title: propTypes.string};
+//Navbar.defaultProps = {
+// title: 'Set title here'
+//};
+// #endregion
+
+export default Navbar;
